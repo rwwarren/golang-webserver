3f633d7492e56972db3bcc7fa2221bd4c3b318b0
Ryan Warren <ryan@war.re>
2015-02-04 23:33:25 -0800

    authserver adding

--- command/authserver/authserver.go
+++ command/authserver/authserver.go
@@ -6,6 +6,10 @@
 package main
 
 import (
+    "net/http"
+    "os"
+    "net"
+    "fmt"
 )
 
 func init() {
@@ -21,13 +25,33 @@ func errorer(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+        ifaces, ipError := net.Interfaces()
+        if ipError != nil {
+          fmt.Println(ipError)
+        }
+        for _, i := range ifaces {
+            addrs, err := i.Addrs()
+            if err != nil {
+              fmt.Println(err)
+            }
+            for _, addr := range addrs {
+                //fmt.Println(_)
+                fmt.Println(addr)
+                switch v := addr.(type) {
+                    case *net.IPAddr:
+                    // process IP address
+                    fmt.Println(v)
+                    //fmt.Println(*net.IPAddr)
+                }
+            }
+        }
 	http.HandleFunc("/get", getPath)
 	http.HandleFunc("/set", setPath)
 	http.HandleFunc("/", errorer)
 	err := http.ListenAndServe(":9090", nil)
 	//err := http.ListenAndServe(portString, nil)
 	if err != nil {
-		log.Errorf("Server Failed: %s", err)
+		//log.Errorf("Server Failed: %s", err)
 		os.Exit(1)
 	}
 }
