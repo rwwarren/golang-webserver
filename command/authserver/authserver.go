4a4c3c30f00bf50f23f5eec2f1b4591262918518
Ryan <warrer@uw.edu>
2015-02-05 01:11:02 -0800

    added ip'

--- command/authserver/authserver.go
+++ command/authserver/authserver.go
@@ -8,8 +8,10 @@ package main
 import (
     "net/http"
     "os"
+    //"os/exec"
     "net"
     "fmt"
+    "strings"
 )
 
 func init() {
@@ -25,7 +27,7 @@ func errorer(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-        ifaces, ipError := net.Interfaces()
+        //ifaces, ipError := net.Interfaces()
         //fmt.Println(net.Interfaces().InterfaceByName("eth0"))
         //fmt.Println(ifaces.InterfaceByName("eth0"))
         ief, err0 := net.InterfaceByName("eth0")
@@ -36,29 +38,61 @@ func main() {
         if err1 !=nil{
                 //log.Fatal(err)
         }
-        fmt.Println("HERE:")
-        fmt.Println(addrs)
+        //fmt.Println("HERE:")
+        //fmt.Println(addrs)
+        //fmt.Println(addrs[0])
+        theIP := fmt.Sprintf("%s", addrs[0])
+        ipAddr := fmt.Sprintf("%s", strings.Split(theIP, "/")[0])
+        fmt.Println(ipAddr)
+        //fmt.Println(strings.Split(theIP, "/"))
+        //fmt.Println(strings.Split(addrs[0].ToString(), "/"))
+        //if ipError != nil {
+        //  fmt.Println(ipError)
+        //}
+        //for _, i := range ifaces {
+        //    addrs, err := i.Addrs()
+        //    if err != nil {
+        //      fmt.Println(err)
+        //    }
+        //    //fmt.Println(addrs.get("eth0"))
+        //    for _, addr := range addrs {
+        //        //fmt.Println(_)
+        //        //fmt.Println(addr)
+        //        switch v := addr.(type) {
+        //            case *net.IPAddr:
+        //            // process IP address
+        //            fmt.Println(v)
+        //            //fmt.Println(*net.IPAddr)
+        //        }
+        //    }
+        //}
+        //addrs, err3 := net.InterfaceAddrs()
 
-        if ipError != nil {
-          fmt.Println(ipError)
-        }
-        for _, i := range ifaces {
-            addrs, err := i.Addrs()
-            if err != nil {
-              fmt.Println(err)
-            }
-            //fmt.Println(addrs.get("eth0"))
-            for _, addr := range addrs {
-                //fmt.Println(_)
-                //fmt.Println(addr)
-                switch v := addr.(type) {
-                    case *net.IPAddr:
-                    // process IP address
-                    fmt.Println(v)
-                    //fmt.Println(*net.IPAddr)
-                }
-            }
-        }
+        // if err3 != nil {
+        //         fmt.Println(err3)
+        //         os.Exit(1)
+        // }
+        // fmt.Println(addrs[*net.IPNet])
+        // //fmt.Println(addrs.(*net.IPNet))
+
+        // for _, address := range addrs {
+
+        //       // check the address type and if it is not a loopback the display it
+        //       if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+        //          if ipnet.IP.To4() != nil {
+        //             fmt.Println(ipnet.IP.String())
+        //          }
+
+        //       }
+        // }
+        //test := "ifconfig"
+        ////test := "ifconfig | grep -A 2 \"eth0\" | grep \"inet addr\" | cut -d: -f2 | awk '{ printf $$1}"
+        //out, err4 := exec.Command(test).Output()
+        //if err4 != nil {
+        //              //fmt.Println("error occured")
+        //              fmt.Printf("%s", err4)
+        //}
+        //fmt.Printf("%s", out)
 	http.HandleFunc("/get", getPath)
 	http.HandleFunc("/set", setPath)
 	http.HandleFunc("/", errorer)
