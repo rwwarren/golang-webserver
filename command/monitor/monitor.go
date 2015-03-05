8db4f3822b6173b1ba89cc76fdeadb5792a66c24
Ryan Warren <ryan@war.re>
2015-03-05 02:14:00 -0800

    updating monitor

--- command/monitor/monitor.go
+++ command/monitor/monitor.go
@@ -4,46 +4,68 @@
 // Monitor it
 //
 // There are a couple flags for this program:
-//--
-//--
-//--
+// --log is the logger configuration file
+// --targets is a comma separated list of urls to monitor
+// --sample-interval-sec interval between samples
+// --runtime-sec runtime of the monitor before it is done
 
 package main
 
 import (
 	log "../../seelog-master/"
-	//"../counter"
 	"flag"
 	"fmt"
 	"net/http"
 	"strings"
 	"time"
+	"io/ioutil"
+	"sync"
 )
+
+// Stores the cookie information
+var concurrentMap struct {
+	sync.RWMutex
+	cookieMap map[string]string
+}
+
+// Initalizes the authserver with the important user storage things
+func init() {
+  // map[string] []Sample
+	concurrentMap = struct {
+		sync.RWMutex
+		cookieMap map[string]string
+	}{cookieMap: make(map[string]string)}
+}
+
+func printResults(){
+	//concurrentMap.RLock()
+	//name := concurrentMap.cookieMap[formCookie]
+	//concurrentMap.RUnlock()
+}
 
 func collectStats(url string, sampleSec int) {
 	for {
 		time.Sleep(time.Duration(sampleSec) * time.Second)
-		//client := http.Client
-		//client := http.Client{
-		//	Timeout: (time.Duration(timeout) * time.Millisecond),
-		//}
-		//response, err := client.Get(url)
 		response, err := http.Get(url)
 		if err != nil {
 			fmt.Println(err)
 		} else {
-			fmt.Println(response)
+			//fmt.Println(response)
+                        fmt.Println(url)
+                        body, _ := ioutil.ReadAll(response.Body)
+                        //body, err := ioutil.ReadAll(response.Body)
+	                respBody := string(body)
+			fmt.Println(respBody)
+			//fmt.Println(response.Body)
 		}
-
 	}
-
 }
 
 // Main function of the loadgen
 func main() {
 	defer log.Flush()
 	logFile := flag.String("log", "logConfig", "This is the logger configuration file")
-	targetList := flag.String("targets", "http://google.com/,http://facebook.com/", "This is the target list of urls")
+	targetList := flag.String("targets", "http://localhost:8080,http://localhost:9090", "This is the target list of urls")
 	sampleInterval := flag.Int("sample-interval-sec", 2, "This is the sample request rate")
 	runtime := flag.Int("runtime-sec", 20, "This is the monitor runtime")
 	flag.Parse()
@@ -68,10 +90,10 @@ func main() {
 	log.Infof("sample-interval-sec Flag: %v", sampleSec)
 	log.Infof("runtime Flag: %v", monitorRuntime)
 	for _, url := range targetsList {
-		//  go run()
-		go collectStats(url, sampleSec)
+                monitorUrl := fmt.Sprintf("%s/monitor", url)
+		go collectStats(monitorUrl, sampleSec)
 	}
 	time.Sleep(time.Duration(monitorRuntime) * time.Second)
-	//printResults()
+	printResults()
 	//TODO make the targets: split it on the commas
 }
