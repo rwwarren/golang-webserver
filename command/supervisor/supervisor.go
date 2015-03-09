c93ffa83706befc2fea8aa5c23510ddf6401bc8b
Ryan Warren <ryan@war.re>
2015-03-09 14:55:39 -0700

    getting it working

--- command/supervisor/supervisor.go
+++ command/supervisor/supervisor.go
@@ -17,25 +17,103 @@ import (
         //"os/exec"
         "strings"
 	//"bytes"
-	//"encoding/json"
+	"encoding/json"
 	"flag"
 	"fmt"
 	//"io/ioutil"
 	//"net/http"
-	//"os"
+	"os"
 	//"strconv"
 	//"strings"
 	//"sync"
 	//"time"
+        "io/ioutil"
 )
 
+//type mytype []app
+type mytype []interface{}
+//type mytype []map[string]interface{}
+
+type myapp struct{
+//  command interface{}
+  //command struct {
+  //    command string
+  //    args string
+  //}
+  //command
+  output string
+  errorString string
+}
+
+type commands struct {
+      Command string
+      Args []string
+  }
+
+type app struct {
+  command []string
+  output string
+  errorString string
+}
+
+type suchapp struct {
+  //Command commands
+  //Command commands
+  Command []string
+  //Command string
+  Output string
+  Error string
+  //command string
+  //output string
+  //errorString string
+}
+//type mytype []map[string]string
 
 // Initalizes the 
 func init() {
 }
 
-func getSupervisionList() []string{
+func getLoadFile(loadingFile string) []byte {
+//func getLoadFile(loadingFile string) string {
+  fileBytes, err := ioutil.ReadFile(loadingFile)
+  if err != nil {
+    log.Criticalf("Failed: %s", err)
+    os.Exit(1)
+  }
+  //TODO leave as byte array?
+  return fileBytes
+  //return string(fileBytes)
+}
 
+func getSupervisionList(loadedFile []byte) []string{
+//func getSupervisionList(loadedFile string) []string{
+
+  //fmt.Println(loadedFile)
+  //var data []mytype
+  var data []suchapp
+  //var data []myapp
+  //var data mytype
+  //var data []app
+  //var data mytype
+  //keys := make([]mytype,0)
+
+  //err := json.Unmarshal(loadedFile, &keys)
+  err := json.Unmarshal(loadedFile, &data)
+  //err := json.Unmarshal([]byte(loadedFile), &data)
+  if err != nil {
+    log.Critical(err)
+  }
+  //var myslice []mytype
+  //DecodeSlicePath(data, &myslice)
+  //fmt.Println(data)
+  for key, val := range data {
+    fmt.Println(key)
+    fmt.Println(val)
+    //first := val.(map[string]interface{})
+    //first := val["commands"].(map[string]interface{})
+    //fmt.Println(first)
+   // fmt.Println(val["command"])
+  }
   return nil
 }
 
@@ -46,6 +124,7 @@ func main() {
 	portRange := flag.String("port-range", "8080-9090", "This is the port range")
 	dumpLoc := flag.String("dumpfile", "backup.bak", "This is the dumpfile")
 	loadFile := flag.String("loadfile", "config.json", "This is the dumpfile")
+	//loadFile := flag.String("loadfile", "config2.json", "This is the dumpfile")
         checkout := flag.Int("checkpoint-interval", 2, "This is the checkpoint interval")
 	flag.Parse()
 	logFileName := fmt.Sprintf("etc/%s.xml", *logFile)
@@ -71,6 +150,8 @@ func main() {
 	log.Infof("dumpfile FLag: %s", dumpfile)
 	log.Infof("checkpoint interval Flag: %v", checkoutInterval)
 	log.Infof("loading file Flag: %s", loadingFile)
-        //supervisionList := getSupervisionList
+        loadedString := getLoadFile(loadingFile)
+        supervisionList := getSupervisionList(loadedString)
+        fmt.Println(supervisionList)
         //strings.Replace on {{port}}
 }
