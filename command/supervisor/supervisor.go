7531ccb1a4280cd8b57b4f43b2a097efdda250e0
Ryan Warren <ryan@war.re>
2015-03-09 15:05:25 -0700

    fmt and json part cleaned up

--- command/supervisor/supervisor.go
+++ command/supervisor/supervisor.go
@@ -13,9 +13,9 @@ package main
 
 import (
 	log "../../seelog-master/"
-        //"os"
-        //"os/exec"
-        "strings"
+	//"os"
+	//"os/exec"
+	"strings"
 	//"bytes"
 	"encoding/json"
 	"flag"
@@ -27,94 +27,39 @@ import (
 	//"strings"
 	//"sync"
 	//"time"
-        "io/ioutil"
+	"io/ioutil"
 )
 
-//type mytype []app
-type mytype []interface{}
-//type mytype []map[string]interface{}
-
-type myapp struct{
-//  command interface{}
-  //command struct {
-  //    command string
-  //    args string
-  //}
-  //command
-  output string
-  errorString string
+type configs struct {
+	Command []string
+	Output  string
+	Error   string
 }
 
-type commands struct {
-      Command string
-      Args []string
-  }
-
-type app struct {
-  command []string
-  output string
-  errorString string
-}
-
-type suchapp struct {
-  //Command commands
-  //Command commands
-  Command []string
-  //Command string
-  Output string
-  Error string
-  //command string
-  //output string
-  //errorString string
-}
-//type mytype []map[string]string
-
-// Initalizes the 
+// Initalizes the
 func init() {
 }
 
 func getLoadFile(loadingFile string) []byte {
-//func getLoadFile(loadingFile string) string {
-  fileBytes, err := ioutil.ReadFile(loadingFile)
-  if err != nil {
-    log.Criticalf("Failed: %s", err)
-    os.Exit(1)
-  }
-  //TODO leave as byte array?
-  return fileBytes
-  //return string(fileBytes)
+	fileBytes, err := ioutil.ReadFile(loadingFile)
+	if err != nil {
+		log.Criticalf("Failed: %s", err)
+		os.Exit(1)
+	}
+	return fileBytes
 }
 
-func getSupervisionList(loadedFile []byte) []string{
-//func getSupervisionList(loadedFile string) []string{
-
-  //fmt.Println(loadedFile)
-  //var data []mytype
-  var data []suchapp
-  //var data []myapp
-  //var data mytype
-  //var data []app
-  //var data mytype
-  //keys := make([]mytype,0)
-
-  //err := json.Unmarshal(loadedFile, &keys)
-  err := json.Unmarshal(loadedFile, &data)
-  //err := json.Unmarshal([]byte(loadedFile), &data)
-  if err != nil {
-    log.Critical(err)
-  }
-  //var myslice []mytype
-  //DecodeSlicePath(data, &myslice)
-  //fmt.Println(data)
-  for key, val := range data {
-    fmt.Println(key)
-    fmt.Println(val)
-    //first := val.(map[string]interface{})
-    //first := val["commands"].(map[string]interface{})
-    //fmt.Println(first)
-   // fmt.Println(val["command"])
-  }
-  return nil
+func getSupervisionList(loadedFile []byte) []configs {
+	var configList []configs
+	err := json.Unmarshal(loadedFile, &configList)
+	if err != nil {
+		log.Critical(err)
+	}
+	for key, val := range configList {
+		fmt.Println(key)
+		fmt.Println(val)
+	}
+	return configList
 }
 
 // Main function of the supervisor
@@ -124,8 +69,7 @@ func main() {
 	portRange := flag.String("port-range", "8080-9090", "This is the port range")
 	dumpLoc := flag.String("dumpfile", "backup.bak", "This is the dumpfile")
 	loadFile := flag.String("loadfile", "config.json", "This is the dumpfile")
-	//loadFile := flag.String("loadfile", "config2.json", "This is the dumpfile")
-        checkout := flag.Int("checkpoint-interval", 2, "This is the checkpoint interval")
+	checkout := flag.Int("checkpoint-interval", 2, "This is the checkpoint interval")
 	flag.Parse()
 	logFileName := fmt.Sprintf("etc/%s.xml", *logFile)
 	logger, logError := log.LoggerFromConfigAsFile(logFileName)
@@ -140,18 +84,18 @@ func main() {
 	log.Warn("Testing warn")
 	log.Error("Testing error")
 	log.Critical("Testing critical")
-        ports := *portRange
+	ports := *portRange
 	portsList := strings.Split(ports, "-")
-        dumpfile := *dumpLoc
-        checkoutInterval := *checkout
-        loadingFile := *loadFile
+	dumpfile := *dumpLoc
+	checkoutInterval := *checkout
+	loadingFile := *loadFile
 	log.Infof("port range Flag: %s", ports)
 	log.Infof("port range list: %s", portsList)
 	log.Infof("dumpfile FLag: %s", dumpfile)
 	log.Infof("checkpoint interval Flag: %v", checkoutInterval)
 	log.Infof("loading file Flag: %s", loadingFile)
-        loadedString := getLoadFile(loadingFile)
-        supervisionList := getSupervisionList(loadedString)
-        fmt.Println(supervisionList)
-        //strings.Replace on {{port}}
+	loadedString := getLoadFile(loadingFile)
+	supervisionList := getSupervisionList(loadedString)
+	fmt.Println(supervisionList)
+	//strings.Replace on {{port}}
 }
