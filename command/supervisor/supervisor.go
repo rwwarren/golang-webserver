003809079e9380cdd4607052c8c9468876511256
Ryan Warren <ryan@war.re>
2015-03-10 18:08:12 -0700

    added comments and code cleanup

--- command/supervisor/supervisor.go
+++ command/supervisor/supervisor.go
@@ -1,45 +1,39 @@
 // (C) Ryan Warren 2015
 // Supervisor
 //
-// Supervisor does....
+// Supervisor monitors servers and makes sure that they
+// are still running by checking every checkpoint-interval
+// seconds and reloads the server if needs be
 //
 // There are a couple flags for this program:
 // --log is the logger configuration file
-// --port-range
+// --port-range is the start and end port of servers
 // --checkpoint-interval interval between checking services
-// --dumpfile sdf
+// --dumpfile is a backup file of the servers that are being
+// monitored by the supervisor
 
 package main
 
 import (
 	log "../../seelog-master/"
-	//"os"
 	"os/exec"
 	"strings"
-	//test
-//	"bytes"
-	//
 	"encoding/json"
 	"flag"
 	"fmt"
-	//"io/ioutil"
-	//"net/http"
 	"os"
 	"strconv"
-	//"strings"
 	"sync"
-	//"time"
 	"io/ioutil"
 	"path/filepath"
-        //"runtime"
 )
 
 // Stores the port information
 var concurrentMap struct {
 	sync.RWMutex
 	portMap []Ports
+        //TODO use size for something
 	size    int
-	//portMap map[int]bool
 }
 
 type Ports struct {
@@ -57,27 +51,24 @@ type configs struct {
         //TODO output all map to JSON then reload it
 }
 
-// Initalizes the
+// Initalizes the concurrent map with the port list
 func init() {
 	concurrentMap = struct {
 		sync.RWMutex
 		portMap []Ports
 		size    int
-		//portMap map[int]bool
-		//}{}
-		//}{portMap: make(map[int]bool)}
 	}{portMap: make([]Ports, 9999)}
 }
 
 //
 func supervise(currentConfig configs, thepath string, wg *sync.WaitGroup) {
+  //TODO check every x seconds, if there sleep
+  // else load server and update
 	//supervise the server
 	size := len(currentConfig.Command) - 1
 	args := make([]string, size)
-	//fmt.Println(size)
         var foundPort string
 	for i := 0; i < size; i++ {
-		//currentCommand := currentConfig.Command[i]
 		currentCommand := currentConfig.Command[i + 1]
 		if strings.Contains(currentCommand, "{{port}}") {
 			foundPort = getFreePort()
@@ -87,37 +78,22 @@ func supervise(currentConfig configs, thepath string, wg *sync.WaitGroup) {
 		fmt.Printf("at this sport: %s\n", currentCommand)
 		args[i] = currentCommand
 	}
-        //attr := new(os.ProcAttr)
-        //cmd, err := os.StartProcess(currentConfig.Command[0], args, attr)
-        //if err != nil {
-        //  log.Critical(err)
-        //}
-        //fmt.Printf("cmd!!! %v \n", cmd)
         program := fmt.Sprintf("%s", currentConfig.Command[0])
-        //program := fmt.Sprintf("%s/%s", thepath, currentConfig.Command[0])
 	cmd := exec.Command(program, args...)
         outfile, outerr := os.Create(currentConfig.Output)
-        //outfile, outerr := os.Create("./out.txt")
         if outerr != nil {
             panic(outerr)
         }
         defer outfile.Close()
         errfile, errerr := os.Create(currentConfig.Error)
-        //outfile, outerr := os.Create("./out.txt")
         if errerr != nil {
             panic(outerr)
         }
         defer errfile.Close()
         //TODO send output to a command line output
-//        cmd.Stdout = os.Stdout
         cmd.Stdout = outfile
         cmd.Stderr = errfile
 	fmt.Println(cmd)
-	//cmd := exec.Command(currentConfig.Command[0], currentConfig.Command[1:size]...)
-	//testing
-	//var out bytes.Buffer
-	//cmd.Stdout = &out
-	//
 	err := cmd.Start()
 	//err := cmd.Run()
 	fmt.Printf("ProcessID: %v\n", cmd.Process.Pid)
@@ -128,33 +104,33 @@ func supervise(currentConfig configs, thepath string, wg *sync.WaitGroup) {
         concurrentMap.Lock()
         currentConfig.PID = cmd.Process.Pid
         currentConfig.CurrentPort = foundPort
+        //TODO backup
+        b, err := json.Marshal(currentConfig)
+        if err != nil {
+            fmt.Println(err)
+        }
+        fmt.Println(string(b))
+        //
         concurrentMap.Unlock()
         wg.Done()
-	//fmt.Printf("in all caps: %q\n", out.String())
-        //output <- out.String()
-//        for {
-//        }
-        //cmd.Wait()
 
 }
 
+// Gets the next free port for the next server to load
 func getFreePort() string {
 	concurrentMap.Lock()
 	var portNum int
 	for i := range concurrentMap.portMap {
 		if !concurrentMap.portMap[i].IsUsed {
-			//fmt.Printf("THIS LOCATION %v \n", i)
 			concurrentMap.portMap[i].IsUsed = true
 			portNum = concurrentMap.portMap[i].PortNumber
 	                concurrentMap.Unlock()
                         fmt.Printf("portnum %v . port string %s \n", portNum, strconv.Itoa(portNum))
-                        //return string(portNum)
                         return strconv.Itoa(portNum)
 		}
 	}
 	concurrentMap.Unlock()
 	return strconv.Itoa(portNum)
-	//return string(portNum)
 }
 
 func buildPorts(ports []string) {
@@ -168,21 +144,14 @@ func buildPorts(ports []string) {
 	}
 	concurrentMap.Lock()
 	total := (max - min)
-	//fmt.Println(total)
-	//myMap := make([]Ports, total)
-	//concurrentMap.portMap = myMap
 	for i := 0; i <= total; i++ {
-		//for currentPort := min; currentPort <= max; currentPort++ {
-		//myMap[i] = Ports{(min + i), false}
 		concurrentMap.portMap[i] = Ports{(min + i), false}
-		//fmt.Println(concurrentMap.portMap[i])
-		//concurrentMap.portMap[currentPort] = false
 	}
 	concurrentMap.size = total
 	concurrentMap.Unlock()
-
 }
 
+//
 func getLoadFile(loadingFile string) []byte {
 	fileBytes, err := ioutil.ReadFile(loadingFile)
 	if err != nil {
@@ -192,17 +161,21 @@ func getLoadFile(loadingFile string) []byte {
 	return fileBytes
 }
 
+// Gets the list of servers to monitor
 func getSupervisionList(loadedFile []byte) []configs {
 	var configList []configs
 	err := json.Unmarshal(loadedFile, &configList)
 	if err != nil {
 		log.Critical(err)
 	}
-	//for key, val := range configList {
-	//	fmt.Println(key)
-	//	fmt.Println(val)
-	//}
 	return configList
+}
+
+func loadBackup(dumpfile string){
+    //TODO load the file
+    // rebuild map
+    // check PIDS 
+    // modify portslist
 }
 
 // Main function of the supervisor
@@ -213,6 +186,7 @@ func main() {
 	dumpLoc := flag.String("dumpfile", "backup.bak", "This is the dumpfile")
         //TODO below change to pipe in from command line
 	loadFile := flag.String("loadfile", "config.json", "This is the dumpfile")
+        //
 	checkout := flag.Int("checkpoint-interval", 2, "This is the checkpoint interval")
 	flag.Parse()
 	logFileName := fmt.Sprintf("etc/%s.xml", *logFile)
@@ -253,35 +227,18 @@ func main() {
 	fmt.Println(thepath)
         //
 
+        //loadBackup(dumpfile)
         wg := new(sync.WaitGroup)
         amount := len(supervisionList)
         wg.Add(amount)
-        //ch := make(chan string)
 	for _, val := range supervisionList {
-		//for key, val := range supervisionList {
-		//fmt.Println(key)
 		fmt.Println(val)
-		//go supervise(val, ch)
-		//supervise(val, ch)
 		go supervise(val, thepath, wg)
-                //go test("/bin/ls")
-                //test("/bin/ls")
-//		supervise(val, thepath)
-		//supervise(val)
 	}
-        //for {
-        //  //fmt.Print(runtime.NumGoroutine())
-
-        //}
-        //for i := range ch {
-        //     fmt.Printf("%v ", i)
-        //}
-
         fmt.Println("Loading the servers")
         wg.Wait()
         fmt.Println("Done! Loaded the servers")
 	//strings.Replace on {{port}}
-        //time.Sleep(5 * time.Second)
         fmt.Println("sleep done")
         //for {
         //}
